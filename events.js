15dd083fa02b1e376b32813c43d98b4d2d651f73
Jerry <jeremy.g.olanda@gmail.com>
2025-08-12 17:33:48 -0600

    Add files via upload

---
+++ events.js
@@ -0,0 +1,632 @@
+const events = [
+  {
+    "title": "Canada Day",
+    "date": "2025-07-01",
+    "description": "Institute closed",
+    "term": "Spring/Summer 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Transfer credit and PLAR applications open",
+    "date": "2025-07-01",
+    "description": "Applications open for fall term",
+    "term": "Spring/Summer 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Final day to apply for transfer credit and PLAR for summer term",
+    "date": "2025-07-02",
+    "description": "Receive tuition refund (most full-time programs)",
+    "term": "Spring/Summer 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Spring/Summer mid-term break",
+    "date": "2025-07-02",
+    "description": "Mid-term break from July 2 to July 4",
+    "term": "Spring/Summer 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Calgary Stampede Parade",
+    "date": "2025-07-04",
+    "description": "Institute closed until 1 pm",
+    "term": "Spring/Summer 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Summer semester commences",
+    "date": "2025-07-07",
+    "description": "",
+    "term": "Spring/Summer 2025",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Spring withdrawal deadline",
+    "date": "2025-07-16",
+    "description": "Deadline for 15-week courses",
+    "term": "Spring/Summer 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Tuition payment deadline - continuing Summer 2025 students",
+    "date": "2025-07-18",
+    "description": "",
+    "term": "Spring/Summer 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Tuition payment deadline - accepted Fall 2025 students starting Aug 25",
+    "date": "2025-07-30",
+    "description": "Check your program page for your start date",
+    "term": "Spring/Summer 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final transcript deadline for fall term applicants",
+    "date": "2025-08-01",
+    "description": "",
+    "term": "Spring/Summer 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Applications open for full-time programs starting fall and summer 2026",
+    "date": "2025-08-01",
+    "description": "For international students",
+    "term": "Spring/Summer 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Civic Holiday",
+    "date": "2025-08-04",
+    "description": "Institute closed",
+    "term": "Spring/Summer 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Tuition payment deadline - accepted Fall 2025 students starting Sept 2",
+    "date": "2025-08-06",
+    "description": "Check your program page for your start date",
+    "term": "Spring/Summer 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Summer withdrawal deadline",
+    "date": "2025-08-09",
+    "description": "Deadline for 8-week courses",
+    "term": "Spring/Summer 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Final exam week",
+    "date": "2025-08-18",
+    "description": "For 15-week spring courses starting week of May 9 (Aug 18-21)",
+    "term": "Spring/Summer 2025",
+    "category": "Exams"
+  },
+  {
+    "title": "End of spring/summer term",
+    "date": "2025-08-21",
+    "description": "",
+    "term": "Spring/Summer 2025",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Add/drop period opens for fall semester",
+    "date": "2025-08-25",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Labour Day",
+    "date": "2025-09-01",
+    "description": "Institute closed",
+    "term": "Fall 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Apply to graduate closed for Fall 2025 convocation",
+    "date": "2025-09-01",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Graduation"
+  },
+  {
+    "title": "Fall classes commence",
+    "date": "2025-09-02",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Apply to graduate open for Winter 2026 convocation",
+    "date": "2025-09-02",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Graduation"
+  },
+  {
+    "title": "Tuition payment deadline - continuing students",
+    "date": "2025-09-12",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final day to apply for transfer credit and PLAR for fall term",
+    "date": "2025-09-12",
+    "description": "Receive tuition refund (most full-time programs)",
+    "term": "Fall 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Add/drop period ends for most full-time programs",
+    "date": "2025-09-12",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Final day to opt-out or add family to Saitsa health and dental plan",
+    "date": "2025-09-12",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "National Day for Truth and Reconciliation",
+    "date": "2025-09-30",
+    "description": "Institute closed",
+    "term": "Fall 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Applications open for full-time programs starting fall and summer 2026 for domestic students",
+    "date": "2025-10-01",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Thanksgiving Day",
+    "date": "2025-10-13",
+    "description": "Institute closed",
+    "term": "Fall 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Self registration opens for Winter 2026 programs",
+    "date": "2025-10-14",
+    "description": "Oct 14 - 17",
+    "term": "Fall 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Open House",
+    "date": "2025-10-17",
+    "description": "Also on Oct 18",
+    "term": "Fall 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Tuition payment deadline - accepted Fall 2025 students starting Nov 24",
+    "date": "2025-10-29",
+    "description": "Check your program page for start date",
+    "term": "Fall 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Mid-term break",
+    "date": "2025-11-10",
+    "description": "Nov 10 - 14",
+    "term": "Fall 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Remembrance Day",
+    "date": "2025-11-11",
+    "description": "Institute closed",
+    "term": "Fall 2025",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Fall 2025 convocation ceremony",
+    "date": "2025-11-13",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Graduation"
+  },
+  {
+    "title": "Withdrawal deadline",
+    "date": "2025-11-14",
+    "description": "For 15-week courses",
+    "term": "Fall 2025",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Final transcript deadline for winter term applicants",
+    "date": "2025-12-01",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Tuition payment deadline - accepted Winter 2026 students starting Jan 5, 2026",
+    "date": "2025-12-10",
+    "description": "Check your program page for your start date",
+    "term": "Fall 2025",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final exam week",
+    "date": "2025-12-12",
+    "description": "Dec 12 to 17, excluding Sunday",
+    "term": "Fall 2025",
+    "category": "Exams"
+  },
+  {
+    "title": "End of fall term",
+    "date": "2025-12-17",
+    "description": "",
+    "term": "Fall 2025",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Winter break",
+    "date": "2025-12-25",
+    "description": "Dec 25 - Jan 1, Institute closed",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "New Year's Day",
+    "date": "2026-01-01",
+    "description": "Institute closed",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Apply to graduate closed for Winter 2026 convocation",
+    "date": "2026-01-01",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Apply to graduate opens for Spring 2026 convocation",
+    "date": "2026-01-02",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Add/drop period for winter begins for most full-time programs",
+    "date": "2026-01-02",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Winter orientation",
+    "date": "2026-01-02",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Winter classes commence",
+    "date": "2026-01-06",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Tuition payment deadline - winter 2026 continuing students",
+    "date": "2026-01-16",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final day to apply for transfer credit or PLAR for winter term",
+    "date": "2026-01-16",
+    "description": "Receive tuition refund (most full-time programs)",
+    "term": "Winter 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Final day to opt-out or add family to Saitsa health and dental plan",
+    "date": "2026-01-16",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Tuition payment deadline for winter 2026 accepted students starting Feb 17",
+    "date": "2026-01-21",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Tuition payment deadline for winter 2026 accepted students starting March 2",
+    "date": "2026-02-04",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Family Day",
+    "date": "2026-02-16",
+    "description": "Institute closed",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Winter mid-term break",
+    "date": "2026-02-16",
+    "description": "Feb 16 - 20",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Tuition payment deadline for winter 2026 accepted students starting March 16",
+    "date": "2026-02-18",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Winter 2026 convocation ceremony",
+    "date": "2026-02-24",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Applications for winter and spring 2027 open",
+    "date": "2026-03-01",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Winter withdrawal deadline",
+    "date": "2026-03-20",
+    "description": "Deadline for 15-week courses",
+    "term": "Winter 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Tuition payment deadline for winter 2026 continuing students starting March 16",
+    "date": "2026-03-27",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Apply to graduate for Spring 2026 closes",
+    "date": "2026-03-31",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Final transcript deadline for spring term applicants",
+    "date": "2026-04-01",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Apply to graduate opens for Fall 2026 conferral",
+    "date": "2026-04-01",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Good Friday",
+    "date": "2026-04-03",
+    "description": "Institute closed",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Easter Monday",
+    "date": "2026-04-06",
+    "description": "Institute closed",
+    "term": "Winter 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Tuition payment deadline for spring 2026 accepted students",
+    "date": "2026-04-06",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final exam week",
+    "date": "2026-04-20",
+    "description": "April 20 - 23",
+    "term": "Winter 2026",
+    "category": "Exams"
+  },
+  {
+    "title": "End of winter term",
+    "date": "2026-04-23",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Add/drop period for spring begins",
+    "date": "2026-04-28",
+    "description": "For most full-time programs",
+    "term": "Winter 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Tuition payment deadline for accepted students starting May 25",
+    "date": "2026-04-29",
+    "description": "",
+    "term": "Winter 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Spring classes commence",
+    "date": "2026-05-05",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Tuition payment deadline - accepted students starting June 8, 2026",
+    "date": "2026-05-13",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Tuition payment deadline - continuing students only",
+    "date": "2026-05-15",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final day to apply for transfer credit or recognition of prior learning (PLAR) for the spring term",
+    "date": "2026-05-15",
+    "description": "Receive a tuition refund (most full-time programs)",
+    "term": "Spring/Summer 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Add/drop period ends for most full-time programs",
+    "date": "2026-05-15",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Apprentice registration opens",
+    "date": "2026-05-15",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Victoria Day",
+    "date": "2026-05-18",
+    "description": "Institute closed",
+    "term": "Spring/Summer 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Final transcript deadline for summer term applicants",
+    "date": "2026-06-01",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Tuition payment deadline - all students starting in July",
+    "date": "2026-06-03",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Spring 2026 convocation ceremonies",
+    "date": "2026-06-16",
+    "description": "June 16 - 18",
+    "term": "Spring/Summer 2026",
+    "category": "Graduation"
+  },
+  {
+    "title": "Spring mid-term break",
+    "date": "2026-06-29",
+    "description": "June 29 - July 3",
+    "term": "Spring/Summer 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Canada Day",
+    "date": "2026-07-01",
+    "description": " Institute closed",
+    "term": "Spring/Summer 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Summer term commences",
+    "date": "2026-07-02",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Start/End of Term"
+  },
+  {
+    "title": "Calgary Stampede Parade",
+    "date": "2026-07-03",
+    "description": "Institute closed until 1 pm",
+    "term": "Spring/Summer 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Spring withdrawal deadline",
+    "date": "2026-07-15",
+    "description": "Deadline for 15-week courses",
+    "term": "Spring/Summer 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Tuition payment deadline - continuing students only",
+    "date": "2026-07-31",
+    "description": "TBD",
+    "term": "Spring/Summer 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Final transcript deadline for fall term applicants",
+    "date": "2026-08-01",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Applications & Admissions"
+  },
+  {
+    "title": "Civic Holiday",
+    "date": "2026-08-03",
+    "description": "Institute closed",
+    "term": "Spring/Summer 2026",
+    "category": "Holidays & Breaks"
+  },
+  {
+    "title": "Tuition payment deadline - all students starting in September",
+    "date": "2026-08-06",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Tuition & Payment"
+  },
+  {
+    "title": "Summer withdrawal deadline",
+    "date": "2026-08-10",
+    "description": "Deadline for 8-week courses",
+    "term": "Spring/Summer 2026",
+    "category": "Registration / Add-Drop"
+  },
+  {
+    "title": "Final exam week",
+    "date": "2026-08-18",
+    "description": "August 18 - 21",
+    "term": "Spring/Summer 2026",
+    "category": "Exams"
+  },
+  {
+    "title": "End of spring/summer term",
+    "date": "2026-08-21",
+    "description": "",
+    "term": "Spring/Summer 2026",
+    "category": "Start/End of Term"
+  }
+];
