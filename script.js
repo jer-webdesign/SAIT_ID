04b10db1a970a37d1c0275bd70bf8a1da348db2a
Jerry <jeremy.g.olanda@gmail.com>
2025-08-13 13:28:00 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const searchInput = document.getElementById("search");
 let eventsData = events; // Use the events array from events.js
 // let activeCategory = "All";
 // let activeCategory = "Applications & Admissions";
-// let activeCategory = "Registration & Enrollment";
-let activeCategory = "Tuition & Payment";
+let activeCategory = "Registration & Enrollment";
+// let activeCategory = "Tuition & Payment";
 let activeTerm = "All";
 let searchTerm = "";
 
